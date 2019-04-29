900acdc7a7834779023c3cfb0a50c8ffee520a51
NeonDactyl <trey@treywestrich.com>
2019-04-28 21:37:50 -0500

    Documentation

--- main.py
+++ main.py
@@ -7,48 +7,56 @@ import threading
 
 class gui:
 
-    def loadWords(self):
+    def load_words(self):
         """
-        Returns a list of valid words. Words are strings of lowercase letters.
-
-        Depending on the size of the word list, this function may
-        take a while to finish.
+        Loads internal list of practice words from file, "wordlist.txt"
         """
-        # inFile: file
-        inFile = open("wordlist.txt", 'r')
+        in_file = open("wordlist.txt", 'r')
         # line: string
-        line = inFile.readline()
+        line = in_file.readline()
         # wordlist: list of strings
-        wordlist = line.split()
-        return wordlist
+        self.word_list = line.split()
 
-    def setWord(self):
+    def set_word(self):
         """
-        Sets the word in the label, and adjusts window geometry
+        Sets the word in the label
         """
-        self.word.set(choice(self.wordlist))
+        self.word.set(choice(self.word_list))
         self.word_label.update()
-        self.window_width = self.word_label.winfo_width() + 60
-        self.window_x_pos = int((self.screen_width - self.window_width) // 2)
-        self.root.geometry("{}x{}+{}+{}".format(self.window_width,
-                                                self.window_height,
-                                                self.window_x_pos,
-                                                self.window_y_pos))
-        self.t = threading.Timer(self.TIMER_LENGTH, self.setWord)
+        self.geometry_update()
+        self.t = threading.Timer(self.TIMER_LENGTH, self.set_word)
         self.start_time = time.time()
         self.time_remaining = self.TIMER_LENGTH
         self.t.start()
 
-    def toggleTimer(self):
+    def geometry_update(self):
+        """
+        Adjusts window size and position to keep it centered on screen, and
+        maintains extra width around the word.
+        """
+        self.window_width = self.word_label.winfo_width() + 60
+        self.window_x_pos = int((self.screen_width - self.window_width) // 2)
+        self.root.geometry("{}x{}+{}+{}".format(self.window_width,
+        self.window_height,
+        self.window_x_pos,
+        self.window_y_pos))
+
+    def toggle_timer(self):
+        """
+        Used to pause and restart the timer, based upon time remaining.
+        """
         if self.t.isAlive():
             self.t.cancel()
             self.time_remaining -= (time.time() - self.start_time)
         else:
-            self.t = threading.Timer(self.time_remaining, self.setWord)
+            self.t = threading.Timer(self.time_remaining, self.set_word)
             self.start_time = time.time()
             self.t.start()
 
-    def onClose(self):
+    def on_close(self):
+        """
+        When closing window, cancel timer, and destroy root.
+        """
         self.t.cancel()
         self.root.destroy()
 
@@ -56,22 +64,26 @@ class gui:
         self.TIMER_LENGTH = timer_length
         self.root = Tk()
         self.root.title("Sight Word Practice")
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+
         self.screen_width = self.root.winfo_screenwidth()
         self.screen_height = self.root.winfo_screenheight()
-        self.window_height = 280
+        self.window_height = 320
         self.window_y_pos = int((self.screen_height - self.window_height) // 2)
+
         self.word = StringVar()
         self.word_label = Label(self.root, textvariable=self.word)
-        self.word_label.grid(column=0, row=0, sticky=(S, E, W), padx=30, pady=30)
+        self.word_label.grid(column=0, row=0, sticky=(E, W), padx=30, pady=30)
         self.word_label.config(font=("Helvetica", 96))
-        self.pause = Button(text="Play/Pause", command=self.toggleTimer)
-        self.pause.grid(row="1", column="0", pady=30, sticky=(N))
-        self.wordlist = self.loadWords()
-        self.setWord()
-        # self.root.geometry("+100+350")
-        self.root.protocol("WM_DELETE_WINDOW", self.onClose)
+
+        self.pause = Button(self.root, text="Pause", command=self.toggle_timer)
+        self.pause.grid(row="1", column="0", sticky=(N))
+        self.pause.config(font=("Helvetica", 32))
+
+        self.load_words()
+        self.set_word()
+
         self.root.mainloop()
 
-
 if __name__ == "__main__":
-    window = gui(8.0)
+    window = gui(3.0)
